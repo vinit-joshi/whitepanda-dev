6f5d0df7da3cb7777f1c6da871c9467be6877bc0
Sushilkumar Shisode <sushilkumar.shisode@webklipper.com>
2019-10-13 20:12:47 +0530

    Fixed accordian and added carousels

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(function() {
-  "use strict";
+    "use strict";
 
-  var nav_offset_top = $('header').height() + 50; 
+    var nav_offset_top = $('header').height() + 50;
     /*-------------------------------------------------------------------------------
-	  Navbar 
-	-------------------------------------------------------------------------------*/
+      Navbar 
+    -------------------------------------------------------------------------------*/
 
-	//* Navbar Fixed  
-    function navbarFixed(){
-        if ( $('.header_area').length ){ 
+    //* Navbar Fixed  
+    function navbarFixed() {
+        if ($('.header_area').length) {
             $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
-                if (scroll >= nav_offset_top ) {
+                var scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
                     $(".header_area").addClass("navbar_fixed");
                 } else {
                     $(".header_area").removeClass("navbar_fixed");
@@ -21,20 +21,56 @@ $(function() {
     };
     //navbarFixed();
 
+    $('#creatorCarousel').on('slide.bs.carousel', function(e) {
+
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 6;
+        var totalItems = $('#creatorCarousel .carousel-item').length;
+
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i = 0; i < it; i++) {
+                // append slides to end
+                if (e.direction == "left") {
+                    $('#creatorCarousel .carousel-item').eq(i).appendTo('#creatorCarousel .carousel-inner');
+                } else {
+                    $('#creatorCarousel .carousel-item').eq(0).appendTo('#creatorCarousel .carousel-inner');
+                }
+            }
+        }
+    });
+
+    $('#genreCarousel.carousel .carousel-item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < 3; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
 
 
-  //------- mailchimp --------//  
-	function mailChimp() {
-		$('#mc_embed_signup').find('form').ajaxChimp();
-	}
-  mailChimp();
+    //------- mailchimp --------//  
+    function mailChimp() {
+        $('#mc_embed_signup').find('form').ajaxChimp();
+    }
+    mailChimp();
 
 
-  $('select').niceSelect();
+    $('select').niceSelect();
 
     /*-------------------------------------------------------------------------------
-	  testimonial slider
-	-------------------------------------------------------------------------------*/
+      testimonial slider
+    -------------------------------------------------------------------------------*/
     if ($('.testimonial').length) {
         $('.testimonial').owlCarousel({
             loop: true,
@@ -56,7 +92,5 @@ $(function() {
 
 
 
-  
+
 });
-
-
